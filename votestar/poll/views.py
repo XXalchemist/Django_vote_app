27dd18fccc8c20588ba68a3dbd7d023b0025f0b0
Narayan Krishna <nkrishn8@gmail.com>
2020-08-05 18:05:36 +0530

    added vote view

--- votestar/poll/views.py
+++ votestar/poll/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question, Choice
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Get Question and display them
 
@@ -16,8 +17,22 @@ def detail(request, question_id):
     except Question.DoesNotExist:
         raise HTTP404('Question Doesnt Exist')
     
-    return render(request, 'poll/results.html',{ 'question': question })
+    return render(request, 'poll/details.html',{ 'question': question })
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'poll/results.html',{'question':question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'poll/details.html',{
+            'question':question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes+=1
+        selected_choice.save()
+        return HttpResponseredirect(reverse('poll:results', args=(question.id,)))
