54b977952742d0980f286cb65d643347944d8172
Narayan Krishna <nkrishn8@gmail.com>
2020-08-05 17:11:16 +0530

    updated index view

--- votestar/poll/views.py
+++ votestar/poll/views.py
@@ -4,6 +4,10 @@ from .models import Question, Choice
 # Get Question and display them
 
 def index(request):
-    return render(request,'poll/index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_questionn_list': latest_question_list
+    }
+    return render(request,'poll/index.html',context)
 
 
