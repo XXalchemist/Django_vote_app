20c8e25d18502b71e8924e6e63031f95d1f0e5b5
Narayan Krishna <nkrishn8@gmail.com>
2020-08-05 17:18:35 +0530

    created index.html

--- votestar/poll/views.py
+++ votestar/poll/views.py
@@ -6,7 +6,7 @@ from .models import Question, Choice
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {
-        'latest_questionn_list': latest_question_list
+        'latest_question_list': latest_question_list
     }
     return render(request,'poll/index.html',context)
 
