9ff14f775ceeaef24e00e6557cb17b330c0e8360
Narayan Krishna <nkrishn8@gmail.com>
2020-08-05 17:25:57 +0530

    added detail view and url

--- votestar/poll/views.py
+++ votestar/poll/views.py
@@ -10,4 +10,10 @@ def index(request):
     }
     return render(request,'poll/index.html',context)
 
-
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise HTTP404('Question Doesnt Exist')
+    
+    return render(request, 'poll/results.html',{ 'question': question })
